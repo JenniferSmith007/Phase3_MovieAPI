92a574f5965eeb5fed72b4a4972d5f09b3989ab5
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-08-18 06:08:05 -0400

    updating for feedback

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,6 +3,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 import { database } from "./movieDatabase";
 
 
+
 class Store {
   constructor(init) {
     const self = this;
@@ -10,12 +11,12 @@ class Store {
     this.subscribers = [];
     database.then(async (db) => {
       this.db = db;
-      const movie = await db.get("moviesToStore", "movie");
+      const favmovie = await db.get("FavmoviesToStore", "favmovie");
       
-      if (movie) {
-        for (const [key, value] of Object.entries(movie)) this.set(key, value);
+      if (favmovie) {
+        for (const [key, value] of Object.entries(favmovie)) this.set(key, value);
       }
-   
+    
     });
     this.state = new Proxy(init, {
       async set(state, key, value) {
@@ -24,19 +25,25 @@ class Store {
         if (self.db) {
           console.log(self.db);
 
-      
-
-          await self.db.add("moviesToStore", 
-          value[value.length - 1])
-
-      
          
+          await self.db.put('FavmoviesToStore',  value[value.length - 1],  key , console.log('putttts',value[value.length - 1]));
+          
+          
+          
+          await self.db.add("FavmoviesToStore", 
+          value[value.length - 1] )
+          console.log(value[value.length - 1])
+         
+         
+
+       
+       
           
         }
         console.log(self.subscribers);
         self.subscribers.forEach((subscriber) => subscriber(state));
 
-        console.log("this is the set method");
+        
       },
     });
   }
@@ -49,9 +56,11 @@ class Store {
 
   addMovie(state, value) {
     let newState = state.movies.push(value);
+    
 
     console.log(value);
     console.log(newState);
+   
 
     this.state = Object.assign(this.state, state);
 
@@ -59,22 +68,63 @@ class Store {
   }
 
   getAllMovies() {
-    return this.state.movies;
+    
+      
+
+    return this.state.movies
   }
-  
+
+  favMovie(state, value) {
+    let favState = state.favmovies.push(value);
+    
+
+    console.log(value);
+    console.log(favState);
+   
+
+    this.state = Object.assign(this.state, state);
+
+    console.log(this.state);
+  }
+
+  getAllFavMovies() {
+    
+      
+
+    return this.state.favmovies
+  }
  
   
 }
-const store = new Store({ movies: [] });
+const store = new Store({ movies: [] } );
+const favstore = new Store({ favmovies: [] } );
+
+
+console.log(store)
+console.log(favstore)
+
+
 
 
 console.log(store.state.movies);
+console.log(favstore.state.favmovies);
+
 
 store.subscribe((state) => {
   console.log(state);
   let movieState = state.movies;
-  console.log('this si movie sta e', movieState)
+  console.log('this si movie state', movieState)
   movieState.forEach((subMovies) => document.body.appendChild(subMovies));
+  
+
+
+});
+
+favstore.subscribe((state) => {
+  console.log(state);
+  let favmovieState = state.favmovies;
+  console.log('this si movie state', favmovieState)
+  favmovieState.forEach((subFavMovies) => document.body.appendChild(subFavMovies));
  
 
 
@@ -86,22 +136,24 @@ class Movies extends HTMLElement {
     this.title = "";
     this.year = "";
     this.plot = "";
+    // do I pass in poster and id for the constructor? 
   }
   static get observedAttributes() {
     return ["title", "year", "plot"];
+    // same as above? 
   }
   attributeChangedCallback(attributeName, oldValue, newValue) {
     if (oldValue === newValue) return;
     this[attributeName] = newValue;
   }
+// why do i not need the connected callback for this custom component HTML?
 
 }
 
 window.customElements.define("movie-component", Movies);
 
-
-
- export async function getData(inputVal, plotLen) {
+async function getData(inputVal, plotLen) {
+ 
   const resp = await fetch(
     `https://www.omdbapi.com/?s=${inputVal}&plot=short&apikey=${apiKey}`
   );
@@ -109,183 +161,165 @@ window.customElements.define("movie-component", Movies);
   console.log(data);
   let searchRes = data.Search;
   console.log(searchRes);
+  console.log(`this is your pltLen: ${plotLen}`);
+
 
   for (let i = 0; i < searchRes.length; i++) {
-    const plotUrl = `https://www.omdbapi.com/?t=${searchRes[i].Title}&plot=${plotLen}&apikey=${apiKey}`;
+    console.log(`this is data: ${searchRes[i].Title}`);
 
+    const plotUrl = `https://www.omdbapi.com/?t=${searchRes[i].Title}&plot=${plotLen}&apikey=${apiKey}`;
+    console.log(`this is your plotURl ${plotUrl}`);
     const plotResp = await fetch(`${plotUrl}`);
     const plotData = await plotResp.json();
-  
-  console.log(plotData)
+    console.log(plotData.Plot);
+    console.log(plotData.Ratings);
+
+
+ 
+    // console.log(Search.Title)
+    let mainCon = document.getElementById("movie");
+    // console.log(data.Search[i])
+    let movie = document.createElement('div')
+    movie.setAttribute("id", "mov")
+    let imgCont = document.createElement("div");
+    let imgTag = document.createElement("img");
+    let movieID = document.createElement("p");
+    let title = document.createElement("p");
+    let year = document.createElement("p");
+    let plot = document.createElement("p");
+
+    imgTag.src = searchRes[i].Poster;
+    imgTag.setAttribute("style", "width: 300px");
+    // how can i use the style in a different file but still use SearchRes value? 
+    //  or how can i style the data from the api call? 
+
+    title.textContent = `${searchRes[i].Title}`;
+    movieID.textContent = `ID - ${searchRes[i].imdbID}`;
+    year.textContent = `YEAR - ${searchRes[i].Year}`;
+    plot.textContent = `DESCRIPTION: ${plotData.Plot}`;
+    plot.setAttribute("id", "plotp")
     
-    let raitingData = plotData.Ratings
-    let movieObject = {
-      Title: searchRes[i].Title,
-      Year: searchRes[i].Year,
-      Plot: plotData.Plot,
-      Poster: searchRes[i].Poster,
-      Rating:raitingData
-    };
+    imgCont.appendChild(imgTag);
+    mainCon.appendChild(movie)
+    movie.appendChild(title);
+    movie.appendChild(movieID);
+    movie.appendChild(year);
+   movie.appendChild(imgCont);
+    movie.appendChild(plot);
+    console.log('this is movie', movie)
+    title.style.textAlign='center'
+    title.style.fontSize='2em'
 
-   let mainCon = document.getElementById("movie");
-      let movie = document.createElement('div')
-      let imgCont = document.createElement("div");
-      let imgTag = document.createElement("img");
-      let movieID = document.createElement("p");
-      let title = document.createElement("p");
-      let year = document.createElement("p");
-      let plot = document.createElement("p");
-      title.setAttribute('id', "movie_title")
-      year.setAttribute('id', "movie_year")
-      plot.setAttribute('id', "movie_plot")
-      imgTag.src = searchRes[i].Poster;
-      imgTag.setAttribute("style", "width: 300px");
-      imgTag.setAttribute("id","movie_poster");
-      
-      title.textContent = `${movieObject.Title}`;
-      
-      movieID.textContent = `ID - ${movieObject.imdbID}`;
-      year.textContent = `YEAR - ${movieObject.Year}`;
-      plot.textContent = `DESCRIPTION: ${movieObject.Plot}`;
-      
-      imgCont.appendChild(imgTag);
-      mainCon.appendChild(movie)
-      movie.appendChild(title);
-      movie.appendChild(movieID);
-      movie.appendChild(year);
-      movie.appendChild(imgCont);
-      movie.appendChild(plot);
-      
-      title.style.textAlign='center'
-      title.style.fontSize='2em'
-      
-      movieID.style.textAlign='center'
-      
-      
-      year.style.textAlign='center'
-      imgCont.style.textAlign='center'
-      
-      let ratings = plotData.Ratings;
-      
-      for (let i = 0; i < ratings.length; i++) {
-        console.log(`${ratings[i].Source} : ${ratings[i].Value}`);
-        let maincon2 = document.getElementById("movie");
-        let raiting = document.createElement("p");
-        raiting.textContent = `RATING: ${ratings[i].Source} : ${ratings[i].Value}`;
-        maincon2.appendChild(raiting);
-        raiting.setAttribute("id", "movie_raiting")
-        raiting.style.color='#646cff'
-        raiting.style.textAlign='center'
-      }
+    movieID.style.textAlign='center'
+   
+   
+   year.style.textAlign='center'
+   imgCont.style.textAlign='center'
 
-        
-      // let favButton = document.createElement("button")
-      // favButton.innerText = 'Favorite <3'
-      // mainCon.appendChild(favButton)
+    let ratings = plotData.Ratings;
+
+    for (let i = 0; i < ratings.length; i++) {
+      console.log(`${ratings[i].Source} : ${ratings[i].Value}`);
+      // let maincon2 = document.getElementById("movie");
+      let raiting = document.createElement("p");
+      raiting.textContent = `RATING: ${ratings[i].Source} : ${ratings[i].Value}`;
+      movie.appendChild(raiting);
+      
+      raiting.style.color='#646cff'
+      raiting.style.textAlign='center'
+    }
+    store.addMovie(store.state, mainCon);
+    console.log('endddd store',store)
     
-      // favButton.addEventListener("click", (e) => {
-      //   let favMovieContainer = document.createElement("div")
-      //   favMovieContainer.setAttribute("id", "fav_movie")
-      //   mainCon.appendChild(favMovieContainer)
+    let favButton = document.createElement("button")
+    console.log(favButton)
+    favButton.setAttribute("id", "favbtn")
+    favButton.textContent = "Favorite <3"
+    movie.appendChild(favButton)
+   
+   
+    favButton.addEventListener("click", (e) => {
+      e.preventDefault(); 
+     let favMovieDisplay = document.createElement("div")
+     
+      let favMovies = document.createElement("div")
+     favMovies.setAttribute("id", "favedmov")
+      favMovieDisplay.setAttribute("id", "favmov")
+     
+
+      let favTitle = document.createTextNode(searchRes[i].Title)
+        let favYear = document.createTextNode(searchRes[i].Year)
+        let favPlot = document.createTextNode(plotData.Plot)
         
-      //   let favmovieObject = {
-      //     Title: searchRes[i].Title,
-      //     Year: searchRes[i].Year,
-      //     Plot: plotData.Plot,
-      //     Poster: searchRes[i].Poster,
-      //     Rating:raitingData, 
-          
-      //   };
-      //   let favratings = plotData.Ratings;
-      
-      //   for (let i = 0; i < favratings.length; i++) {
-      //     console.log(`${favratings[i].Source} : ${favratings[i].Value}`);
-      //     let favraiting = document.createElement("p");
-      //     favraiting.textContent = `RATING: ${favratings[i].Source} : ${favratings[i].Value}`;
-      //    favMovieContainer.appendChild(favraiting);
-      //    favraiting.setAttribute("id", "movie_raiting")
-      //    favraiting.style.color='#646cff'
-      //    favraiting.style.textAlign='center'
-      //   }
-
-
-
-
-
-
-
-      //   let favTitle = document.createTextNode(favmovieObject.Title)
-      //   let favYear = document.createTextNode(favmovieObject.Year)
-      //   let favPlot = document.createTextNode(favmovieObject.Plot)
-      //   let favPoster = document.createElement('img')
-      //   favPoster.src = favmovieObject.Poster 
+        let favMovieID = document.createTextNode(searchRes[i].imdbID)
+        let favPoster = document.createElement('img')
+        favPoster.src = searchRes[i].Poster 
         
         
-        
-      //   favMovieContainer.appendChild(favTitle)
-      //   favMovieContainer.appendChild(favYear)
-      //   favMovieContainer.appendChild(favPlot)
-      //   favMovieContainer.appendChild(favPoster)
-      //   // favMovieContainer.appendChild(favraiting)
-        
-
-        
-      //   favstore.addFavMovie(favstore.state, favMovieContainer );
-      
-
-
-    
-      // let notesButton = document.createElement("button")
-      // notesButton.innerText = "Add A Note"
-      // mainCon.appendChild(notesButton)
-      
-  
-      // notesButton.addEventListener("click", (e) => {
-      //   let notesDiv = document.createElement("div")
-      //  let notesInput = document.createElement("textarea")
-      //   notesInput.setAttribute("rows", "15")
-      //   notesInput.setAttribute("cols", "35")
-      //   notesInput.setAttribute("id", "notes")
-      //   let addNote = document.createElement("button")
-      //     addNote.innerText = 'Submit'
-        
-      //   // let notesVal = document.getElementById("notes").value
-      //   // console.log(notesVal)
-      //   notesDiv.appendChild(notesInput)
-      //   notesDiv.appendChild(addNote)
-      
-      
-      
-      
-      
-      //   mainCon.appendChild(notesDiv)
-      
-  
-        
-  
-  
-  
-      // })
-
-
-
+        favMovieDisplay.appendChild(favMovies)
+        favMovies.appendChild(favTitle)
+        favMovies.appendChild(favYear)
+        favMovies.appendChild(favPoster)
+        favMovies.appendChild(favPoster)
+        favMovies.appendChild(favMovieID)
+        favMovies.appendChild(favPlot)
+        // favMovieContainer.appendChild(favraiting)
+        document.body.appendChild(favMovieDisplay)
+        console.log(favMovieDisplay)
+        let favedMovies = []
+        favedMovies.push(favMovies)
+        console.log(favedMovies)
        
+        
+        // let searchResObj = {
+        //   Ttile: `${searchRes[i].Title}`,
+        //   plot: `${searchRes[i].Plot}`,
+        //   comments: `${noteSec}`
+        // }
+           
+        favstore.favMovie(favstore.state,searchRes[i]);
+         
+    console.log('endddd store',favstore)
+    })
 
 
 
 
-    console.log("this is movie obj --", movieObject);
-    store.addMovie(store.state, movieObject );
+    let notesbtn = document.createElement("button")
+    notesbtn.textContent = "Notes"
+    movie.appendChild(notesbtn)
+
+    notesbtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let noteSec = document.createElement("div")
+      let notes = document.createElement("textarea")
+      let addNote = document.createElement("button")
+      addNote.textContent = "add a Note"
+      notes.setAttribute("cols", "30")
+      notes.setAttribute("rows", "10")
+      notes.setAttribute("id", "usernotes")
+      movie.appendChild(noteSec)
+      noteSec.appendChild(notes)
+      noteSec.appendChild(addNote)
+      addNote.addEventListener("click", (e) => {
+        e.preventDefault(); 
+        let notesres = document.createElement("p")
+        let notesVal = document.getElementById("usernotes").value
+        let notesValNode = document.createTextNode(notesVal)
+        console.log(notesVal)
+        notesres.appendChild(notesValNode)
+        noteSec.appendChild(notesres)
+        
+      })
+    })
+   
     
+    
+
+
+
   }
-
-
-
-
-
-
 }
-
 
 
 const button = document.getElementById("button");
@@ -298,11 +332,9 @@ button.addEventListener("click", function (e) {
   );
   const plotLen = plot.value;
   console.log(`this is plot lenth 3: ${plotLen}`);
-  getData(inputVal, plotLen)
-  console.log('this is datttttaaaa', {getData})
+  getData(inputVal, plotLen);
 
-  
-
+ 
 });
 
 
--- vite-project/movieDatabase.js
+++ vite-project/movieDatabase.js
@@ -1,20 +1,22 @@
 import { openDB } from 'idb';
-const apiKey = import.meta.env.VITE_API_KEY;
+
 
 export const database =  openDB('MovieStore', 2, {
+  
     upgrade(db) {
-     db.createObjectStore('moviesToStore',    {keyPath:"id",
      
-       autoIncrement: true });
+  
+     db.createObjectStore('FavmoviesToStore')
 
-       db.createObjectStore('FavmoviesToStore',    {keyPath:"fav",
      
-       autoIncrement: true });
-      
+    
+    
+     
       
     },
+    
   });
 
 
-  
+
   
